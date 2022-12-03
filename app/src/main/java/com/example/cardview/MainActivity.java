06df7ca6c7237d38b93033863fd689987ef16ff0
Mithun2929 <kaththimithun@gmail.com>
2022-12-03 21:47:35 +0530

    full commit

---
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public CardView card1,card2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        card1=(CardView) findViewById(R.id.c1);
+        card2=(CardView) findViewById(R.id.c2);
+
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+
+        switch (view.getId()){
+            case R.id.c1:
+                i = new Intent(this,one.class);
+                startActivity(i);
+                break;
+
+            case R.id.c2:
+                i = new Intent(this,two.class);
+                startActivity(i);
+                break;
+        }
+
+    }
+}
